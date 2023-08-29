98b6876c1e955fcb00418a0160b93275dc1bdfa1
arjunsavel <asavel@gmail.com>
2023-08-29 15:18:32 -0400

    reformat package structure

---
+++ src/astrotemplate/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__uri__ = "https://github.com/astrodbtoolkit/astrotemplate-db"
+__author__ = "astrodbtoolkit team"
+__email__ = "asavel@gmail.com"
+__license__ = "BSD 3-Clause License"
+__version__ = "0.0.1-beta"
+__release__ = "0.0.1-beta"
+__description__ = "astrotemplate-db: a template for astronomocal databases"
