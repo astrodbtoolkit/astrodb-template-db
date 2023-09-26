a80a97ce62f01aae4163f9388903600baf1b3dfc
arjunsavel <asavel@gmail.com>
2023-09-26 16:01:48 -0400

    add all the tables

--- src/astrotemplate/scripts/intitialize_utils.py
+++ src/astrotemplate/scripts/intitialize_utils.py
@@ -1,5 +1,7 @@
 """
 Utility functions for initializing a database, including initial table creation.
+
+test: check that the foreign key constraints match the tables to exist
 """
 from astrodbkit2.astrodb import create_database, Database
 from astrotemplate.schema import *
@@ -63,30 +65,48 @@ class TableInitializer(object):
         self.db.add_table_data(table_data_path, table=self.table_name, fmt='csv')
         self.db.save_database(abs_path + '/data')
 
-def SourceTableInitializer(TableInitializer):
-    table_name = 'Sources'
-    def __init__(self):
-        super().__init__()
+
 
 def PublicationsTableInitializer(TableInitializer):
     table_name = 'Publications'
-    def __init__(self):
-        super().__init__()
+
+def SourceTableInitializer(TableInitializer):
+    table_name = 'Sources'
+    tables_to_exist = ['Publications']
+
+def NamesTableInitializer(TableInitializer):
+    table_name = 'Names'
+    tables_to_exist = ['Sources']
+
+def ModesTableInitializer(TableInitializer):
+    table_name = 'Modes'
+    tables_to_exist = ['Instruments', 'Telescopes']
+
+def RegimesTableInitializer(TableInitializer):
+    table_name = 'Regimes'
+
+def PhotometryFiltersTableInitializer(TableInitializer):
+    table_name = 'PhotometryFilters'
+    tables_to_exist = ['Instruments', 'Telescopes']
 
 def InstrumentsTableInitializer(TableInitializer):
     table_name = 'Instruments'
-    def __init__(self):
-        super().__init__()
+    tables_to_exist = ['Publications']
 
 def TelescopesTableInitializer(TableInitializer):
     table_name = 'Telescopes'
-    def __init__(self):
-        super().__init__()
+    tables_to_exist = ['Publications']
 
 def SpectraTableInitializer(TableInitializer):
     table_name = 'Spectra'
     tables_to_exist = ['Sources', 'Publications', 'Instruments', 'Telescopes']
-    def __init__(self):
-        super().__init__()
+
+    def initialize_table(table_data_path):
+
+        # first, check whether the URL exists when read in from the CSV
+        # second, cast the date times as correct
+        # finally, add the data to the table as usual
+        super().initialize_table(table_data_path)
+
 
 
