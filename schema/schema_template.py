56f0024f3deaa4b7867cab1cd47d9789309efb59
kelle <kellecruz@gmail.com>
2024-01-25 14:36:42 -0500

    template schema tweaks and rename

--- schema/schema_template.py
+++ schema/schema_template.py
@@ -1,4 +1,4 @@
-# Schema for the SIMPLE database
+# Schema for the AstroDD Template database
 
 from sqlalchemy import (
     Column,
@@ -6,24 +6,25 @@ from sqlalchemy import (
     ForeignKey,
     Integer,
     String,
-    Enum,
     DateTime,
-    ForeignKeyConstraint,
 )
 import enum
 from astrodbkit2.astrodb import Base
-from sqlalchemy.orm import Mapped
 
 
-# todo: make "tabulardata" or "physicaldata" abstract classes.
+# TODO: make "tabulardata" or "physicaldata" abstract classes.
+
+
 # -------------------------------------------------------------------------------------------------------------------
 # Reference tables
+# -------------------------------------------------------------------------------------------------------------------
 class Publications(Base):
     """ORM for publications table.
     This stores reference information (DOI, bibcodes, etc) and
     has shortname as the primary key
     """
-    __tablename__ = 'Publications'
+
+    __tablename__ = "Publications"
     reference = Column(String(30), primary_key=True, nullable=False)
     bibcode = Column(String(100))
     doi = Column(String(100))
@@ -31,23 +32,29 @@ class Publications(Base):
 
 
 class Telescopes(Base):
-    __tablename__ = 'Telescopes'
+    __tablename__ = "Telescopes"
     telescope = Column(String(30), primary_key=True, nullable=False)
     description = Column(String(1000))
-    reference = Column(String(30), ForeignKey('Publications.reference',
-                                              onupdate='cascade'))
+    reference = Column(
+        String(30), ForeignKey("Publications.reference", onupdate="cascade")
+    )
 
 
 class Instruments(Base):
-    __tablename__ = 'Instruments'
+    __tablename__ = "Instruments"
     instrument = Column(String(30), primary_key=True, nullable=False)
     mode = Column(String(30), primary_key=True)
-    telescope = Column(String(30),
-                       ForeignKey('Telescopes.telescope', onupdate='cascade'),
-                       primary_key=True)
+    telescope = Column(
+        String(30),
+        ForeignKey("Telescopes.telescope", onupdate="cascade"),
+        primary_key=True,
+    )
     description = Column(String(1000))
-    reference = Column(String(30),
-                       ForeignKey('Publications.reference', onupdate='cascade'))
+    reference = Column(
+        String(30), ForeignKey("Publications.reference", onupdate="cascade")
+    )
+
+
 # instead of having modes...telescope --> instrument --> mode (used mostly for spectra).
 # think about SVO.
 
@@ -58,15 +65,20 @@ class PhotometryFilters(Base):
     This stores relationships between filters and instruments,
     telescopes, as well as wavelength and width
     """
-    __tablename__ = 'PhotometryFilters'
+
+    __tablename__ = "PhotometryFilters"
     band = Column(String(30), primary_key=True, nullable=False)
     # of the form instrument.filter (see SVO)
-    instrument = Column(String(30),
-                        ForeignKey('Instruments.instrument', onupdate='cascade'),
-                        primary_key=True)
-    telescope = Column(String(30),
-                       ForeignKey('Telescopes.telescope', onupdate='cascade'),
-                       primary_key=True)
+    instrument = Column(
+        String(30),
+        ForeignKey("Instruments.instrument", onupdate="cascade"),
+        primary_key=True,
+    )
+    telescope = Column(
+        String(30),
+        ForeignKey("Telescopes.telescope", onupdate="cascade"),
+        primary_key=True,
+    )
     effective_wavelength = Column(Float, nullable=False)
     width = Column(Float)
 
@@ -76,7 +88,8 @@ class Versions(Base):
     ORM for Versions table
     This stores the version numbers for the database
     """
-    __tablename__ = 'Versions'
+
+    __tablename__ = "Versions"
     version = Column(String(30), primary_key=True, nullable=False)
     start_date = Column(String(30))
     end_date = Column(String(30))
@@ -86,23 +99,25 @@ class Versions(Base):
 # -------------------------------------------------------------------------------------------------------------------
 # Hard-coded enumerations
 
+
 class Regime(enum.Enum):
     """Enumeration for spectral type, spectra, and photometry regimes
     Use UCD controlled vocabulary:
     https://www.ivoa.net/documents/UCD1+/20200212/PEN-UCDlist-1.4-20200212.html#tth_sEcB
     The string values are used, not the variable names.
     """
-    ultraviolet = 'em.UV'
-    optical_UCD = 'em.opt'
-    optical = 'optical'
-    nir_UCD = 'em.IR.NIR'  # Near-Infrared, 1-5 microns
-    nir = 'nir'
-    infrared = 'em.IR'  # Infrared part of the spectrum
-    mir_UCD = 'em.IR.MIR'  # Medium-Infrared, 5-30 microns
-    mir = 'mir'
-    millimeter = 'em.mm'
-    radio = 'em.radio'
-    unknown = 'unknown'
+
+    ultraviolet = "em.UV"
+    optical_UCD = "em.opt"
+    optical = "optical"
+    nir_UCD = "em.IR.NIR"  # Near-Infrared, 1-5 microns
+    nir = "nir"
+    infrared = "em.IR"  # Infrared part of the spectrum
+    mir_UCD = "em.IR.MIR"  # Medium-Infrared, 5-30 microns
+    mir = "mir"
+    millimeter = "em.mm"
+    radio = "em.radio"
+    unknown = "unknown"
 
 
 # -------------------------------------------------------------------------------------------------------------------
@@ -110,28 +125,33 @@ class Regime(enum.Enum):
 class Sources(Base):
     """ORM for the sources table.
     This stores the main identifiers for our objects along with ra and dec"""
-    __tablename__ = 'Sources'
+
+    __tablename__ = "Sources"
     source = Column(String(100), primary_key=True, nullable=False)
     ra = Column(Float)
     dec = Column(Float)
     epoch = Column(Float)  # decimal year
     equinox = Column(String(10))  # eg, J2000
     shortname = Column(String(30))  # not needed?
-    reference = Column(String(30),
-                       ForeignKey('Publications.reference',
-                                  onupdate='cascade'),
-                       nullable=False)
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", onupdate="cascade"),
+        nullable=False,
+    )
     other_references = Column(String(100))
     comments = Column(String(1000))
 
 
 class Names(Base):
-    __tablename__ = 'Names'
-    source = Column(String(100), ForeignKey('Sources.source',
-                                            ondelete='cascade',
-                                            onupdate='cascade'),
-                    nullable=False, primary_key=True)
+    __tablename__ = "Names"
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
     other_name = Column(String(100), primary_key=True, nullable=False)
+
 
 # todo: make "tabulardata" or "physicaldata" abstract classes.
 
@@ -147,15 +167,18 @@ class _DataPointerTable:
 
 
 class Spectra(_DataPointerTable, Base):
-    __tablename__ = 'Spectra'
-    source = Column(String(100), ForeignKey('Sources.source',
-                                            ondelete='cascade',
-                                            onupdate='cascade'),
-                    nullable=False, primary_key=True)
-    reference = Column(String(30), ForeignKey('Publications.reference',
-                                              ondelete='cascade',
-                                              onupdate='cascade'),
-                       primary_key=True)
+    __tablename__ = "Spectra"
+    source = Column(
+        String(100),
+        ForeignKey("Sources.source", ondelete="cascade", onupdate="cascade"),
+        nullable=False,
+        primary_key=True,
+    )
+    reference = Column(
+        String(30),
+        ForeignKey("Publications.reference", ondelete="cascade", onupdate="cascade"),
+        primary_key=True,
+    )
     # Data
     spectrum = Column(String(1000), nullable=False)  # URL of spectrum location
     # URL of original spectrum location, if applicable
@@ -168,12 +191,18 @@ class Spectra(_DataPointerTable, Base):
     # values_callable=lambda x: [e.value for e in x],
     #                      native_enum=False),
     #                 )  # eg, Optical, Infrared, etc
-    telescope = Column(String(30),
-                       ForeignKey('Telescopes.telescope'), onupdate='cascade',
-                       primary_key=True)
-    instrument = Column(String(30),
-                        ForeignKey('Instruments.instrument'), onupdate='cascade',
-                        primary_key=True)
+    telescope = Column(
+        String(30),
+        ForeignKey("Telescopes.telescope"),
+        onupdate="cascade",
+        primary_key=True,
+    )
+    instrument = Column(
+        String(30),
+        ForeignKey("Instruments.instrument"),
+        onupdate="cascade",
+        primary_key=True,
+    )
     mode = Column(String(30))  # eg, Prism, Echelle, etc
     observation_date = Column(DateTime)
     wavelength_units = Column(String(20))
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,17 +1,25 @@
 """
 functions to test the schema itself.
 """
-
 import pytest
 import os
-print(os.getcwd())
-from schema.schema import *
-DB_NAME = 'test.db'
-DB_PATH = 'data'
-
-
+from schema.schema_template import *
 from astrodbkit2.astrodb import create_database, Database
-REFERENCE_TABLES = ['Publications', 'Telescopes', 'Instruments', 'Modes', 'PhotometryFilters', 'Versions', 'Parameters']
+
+
+DB_NAME = "test.sqlite"
+DB_PATH = "data"
+
+REFERENCE_TABLES = [
+    "Publications",
+    "Telescopes",
+    "Instruments",
+    "Modes",
+    "PhotometryFilters",
+    "Versions",
+    "Parameters",
+]
+
 
 # Load the database for use in individual tests
 @pytest.fixture(scope="module")
@@ -21,28 +29,35 @@ def db():
 
     if os.path.exists(DB_NAME):
         os.remove(DB_NAME)
-    connection_string = 'sqlite:///' + DB_NAME
+    connection_string = "sqlite:///" + DB_NAME
     create_database(connection_string)
     assert os.path.exists(DB_NAME)
 
     # Connect to the new database and confirm it has the Sources table
     db = Database(connection_string, reference_tables=REFERENCE_TABLES)
     assert db
-    assert 'source' in [c.name for c in db.Sources.columns]
+    assert "source" in [c.name for c in db.Sources.columns]
 
     return db
 
 
 def test_setup_db(db):
     # Some setup tasks to ensure some data exists in the database first
-    ref_data = [{'reference': 'Ref 1', 'doi': '10.1093/mnras/staa1522', 'bibcode': '2020MNRAS.496.1922B'},
-                {'reference': 'Ref 2', 'doi': 'Doi2', 'bibcode': '2012yCat.2311....0C'},
-                {'reference': 'Burn08', 'doi': 'Doi3', 'bibcode': '2008MNRAS.391..320B'}]
+    ref_data = [
+        {
+            "reference": "Ref 1",
+            "doi": "10.1093/mnras/staa1522",
+            "bibcode": "2020MNRAS.496.1922B",
+        },
+        {"reference": "Ref 2", "doi": "Doi2", "bibcode": "2012yCat.2311....0C"},
+        {"reference": "Burn08", "doi": "Doi3", "bibcode": "2008MNRAS.391..320B"},
+    ]
 
-    source_data = [{'source': 'Fake 1', 'ra': 9.0673755, 'dec': 18.352889, 'reference': 'Ref 1'},
-                   {'source': 'Fake 2', 'ra': 9.0673755, 'dec': 18.352889, 'reference': 'Ref 1'},
-                   {'source': 'Fake 3', 'ra': 9.0673755, 'dec': 18.352889, 'reference': 'Ref 2'},
-                   ]
+    source_data = [
+        {"source": "Fake 1", "ra": 9.0673755, "dec": 18.352889, "reference": "Ref 1"},
+        {"source": "Fake 2", "ra": 9.0673755, "dec": 18.352889, "reference": "Ref 1"},
+        {"source": "Fake 3", "ra": 9.0673755, "dec": 18.352889, "reference": "Ref 2"},
+    ]
 
     with db.engine.connect() as conn:
         conn.execute(db.Publications.insert().values(ref_data))
