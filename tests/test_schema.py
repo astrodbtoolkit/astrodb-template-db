497e6aa9c72d4208983c8bea813e2e2e6d77c811
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2024-04-30 17:02:12 -0400

    add a missing double quotw

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -194,7 +194,7 @@ def test_versions(db):
 @pytest.mark.parametrize("values, error_state",
                          [
                              ({"source": "ThisIsASuperLongSourceNameThatIsInvalid", "other_name": "OtherName"}, ValueError),
-                             ({"source": None, other_name="OtherName}, ValueError),
+                             ({"source": None, other_name="OtherName"}, ValueError),
                              ({"source": "Source", other_name="ThisIsASuperLongOtherNameThatIsInvalid}, ValueError),
                              ({"telescope": "Source", "other_name": None}, ValueError)
                           ])
