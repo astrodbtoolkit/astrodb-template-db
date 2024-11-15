5cc3cdc009bfd9aa619f8d371c7ece4bd1087751
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2024-11-15 11:12:05 +0000

    iterating on tests

--- tests/test_felis.py
+++ tests/test_felis.py
@@ -4,7 +4,9 @@ import pytest
 import yaml
 import sqlalchemy as sa
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from pydantic import ValidationError
+from astrodbkit.astrodb import AstrodbQuery
 
 from felis.datamodel import Schema
 from felis.metadata import MetaDataBuilder
@@ -16,8 +18,8 @@ def schema():
     schema = Schema.model_validate(data)
     return schema
 
-
-def test_database(schema):
+@pytest.fixture()
+def db_object(schema):
     # Build in-memory database
     metadata = MetaDataBuilder(schema).build()
 
@@ -28,9 +30,33 @@ def test_database(schema):
         conn.execute(sa.text("ATTACH ':memory:' AS astrodb"))
 
     metadata.create_all(engine)
-
+    return engine, metadata
 
 # TODO: Confirm ORM usage
 # TODO: Check constraints
 
+def test_inserts(db_object):
+    # Attempt insert with ORM
 
+    engine, metadata = db_object
+    
+    with engine.connect() as conn:
+        metadata.reflect(conn)
+
+    # This requires us to use the DB name (astrodb)
+    Sources = metadata.tables['astrodb.Sources']
+    source_data = [
+        {"source": "Fake 1", "ra_deg": 9.0673755, },
+    ]
+
+    with engine.connect() as conn:
+        conn.execute(Sources.insert().values(source_data))
+        conn.commit()
+
+    # Explicitly with ORM
+    # Does not work because the Table object (Sources) is not callable
+    # Session = sessionmaker(bind=engine, query_cls=AstrodbQuery)
+    # s = Sources(source="V4046 Sgr", ra_deg=273.54, dec_deg=-32.79, reference="Ref 1")
+    # with Session() as session:
+    #     session.add(s)
+    #     session.commit()
