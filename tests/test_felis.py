5647135bf368d6c6a3e4764375068dd9c78709eb
kelle <kellecruz@gmail.com>
2024-12-09 22:48:58 -0500

    add PhotometryFilters table

--- tests/test_felis.py
+++ tests/test_felis.py
@@ -186,14 +186,13 @@ def test_queries(db_object):
     db = db_object
 
     # Confirm the right tables are present
-    assert len(db.metadata.tables.keys()) == 5
+    assert len(db.metadata.tables.keys()) == 6
     assert "Sources" in db.metadata.tables.keys()
     assert "Publications" in db.metadata.tables.keys()
     assert "Names" in db.metadata.tables.keys()
     assert "Telescopes" in db.metadata.tables.keys()
     assert "Instruments" in db.metadata.tables.keys()
-
-
+    assert "PhotometryFilters" in db.metadata.tables.keys()
 
     # print(db.query(db.Sources).table())
     # print(db.sql_query("select * from Sources", fmt="astropy"))
