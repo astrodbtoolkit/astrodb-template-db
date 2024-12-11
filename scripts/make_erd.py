a5fffc874511f4bd710fb565dd7bf316f467f93e
David Rodriguez <drodriguez@stsci.edu>
2024-12-11 11:40:27 -0500

    Updating ER diagram script to use Felis schema

--- scripts/make_erd.py
+++ scripts/make_erd.py
@@ -6,11 +6,10 @@ from astrodbkit.astrodb import Database, create_database
 from eralchemy2 import render_er
 
 sys.path.append("./")  # needed for github actions to find the template module
-from schema.schema_template import *
 from schema.schema_template import REFERENCE_TABLES
 
 # Connect to an in-memory sqlite database
-create_database(connection_string="sqlite://")
+create_database(connection_string="sqlite://", felis_schema="schema/schema.yaml")
 db = Database("sqlite://", reference_tables=REFERENCE_TABLES)
 
 # Create ER model from the database metadata
