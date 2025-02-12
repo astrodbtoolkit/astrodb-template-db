9c9da56f630d0e4bb0499f1284e14cb20106b76e
kelle <kellecruz@gmail.com>
2025-02-12 16:20:38 -0500

    add links to script for later use

--- scripts/ingest_gl229b.py
+++ scripts/ingest_gl229b.py
@@ -1,3 +1,9 @@
+# This script ingests data related to Gl 229b into the database
+# Relevant websites to use for future ingestions:
+# Gl 229: https://simbad.cds.unistra.fr/simbad/sim-id?Ident=Gl+229
+# Gl 229b: https://simbad.cds.unistra.fr/simbad/sim-id?Ident=Gl+229b
+# Gl 229b in SIMPLE: https://simple-bd-archive.org/load_solo/Gl%20229B
+
 from astrodb_utils import load_astrodb
 from astrodb_utils.sources import ingest_source
 from astrodb_utils.publications import ingest_publication
@@ -6,7 +12,6 @@ from astrodb_utils.publications import ingest_publication
 DB_NAME = "tests/astrodb_template_tests.sqlite"
 SCHEMA_PATH = "schema/schema.yaml"
 db = load_astrodb(DB_NAME, recreatedb=True, felis_schema=SCHEMA_PATH)
-
 
 ingest_publication(db, doi="10.1038/378463a0")
 
