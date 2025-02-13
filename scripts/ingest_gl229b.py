bfa05d3060144b09ce4ca6fd86bafbf75db6acdb
kelle <kellecruz@gmail.com>
2025-02-13 13:28:49 -0500

    add template data

--- scripts/ingest_gl229b.py
+++ scripts/ingest_gl229b.py
@@ -13,24 +13,37 @@ DB_NAME = "tests/astrodb_template_tests.sqlite"
 SCHEMA_PATH = "schema/schema.yaml"
 
 REFERENCE_TABLES = [
-        "Publications",
-        "Telescopes",
-        "Instruments",
-        "Versions",
-        "PhotometryFilters",
-        "Regimes",
-        "AssociationList",
-        "ParameterList",
-    ]
+    "Publications",
+    "Telescopes",
+    "Instruments",
+    "Versions",
+    "PhotometryFilters",
+    "Regimes",
+    "AssociationList",
+    "ParameterList",
+    "CompanionList",
+]
 
 db = load_astrodb(DB_NAME, recreatedb=True, felis_schema=SCHEMA_PATH, reference_tables=REFERENCE_TABLES)
 
 def ingest_gl229b(db):
-    ingest_publication(db, doi="10.1038/378463a0")
+    # ingest_publication(db, doi="10.1038/378463a0")
     ingest_source(db, "Gl 229b", reference="Naka95")
 
 
 def ingest_companion(db):
+
+    gl229_data = [
+        {
+            "companion": "Gl 229",
+        }
+    ]
+    with db.engine.connect() as conn:
+        conn.execute(db.CompanionList.insert().values(gl229_data))
+        conn.commit()
+
+
+def ingest_companion_data(db):
     companion_data = [
         {
             "source": "Gl 229b",
@@ -59,14 +72,14 @@ def ingest_age_parameter(db):
 
 
 def ingest_gl229_parameters(db):
-    ingest_publication(
-        db,
-        doi="10.1093/mnras/stad343",
-        bibcode="2023MNRAS.520.5283G",
-        reference="Gaid23",
-        description="The TIME Table: rotation and ages of cool exoplanet host stars",
-        ignore_ads=True,
-    )
+    # ingest_publication(
+    #     db,
+    #     doi="10.1093/mnras/stad343",
+    #     bibcode="2023MNRAS.520.5283G",
+    #     reference="Gaid23",
+    #     description="The TIME Table: rotation and ages of cool exoplanet host stars",
+    #     ignore_ads=True,
+    # )
 
     gl229b_data = [      
         {
@@ -86,10 +99,11 @@ def ingest_gl229_parameters(db):
 
 
 DB_SAVE = True
-# ingest_gl229b(db)
-# ingest_age_parameter(db)
-ingest_age_parameter(db)
-ingest_gl229_parameters(db)
+ingest_gl229b(db)  # add to Sources table
+# ingest_age_parameter(db)  # add to ParameterList table
+ingest_companion(db)  # add to CompanionList table
+ingest_gl229_parameters(db)  # add to CompanionParameters table
+ingest_companion_data(db)  # add to CompanionRelationships table
 
 if DB_SAVE:
     db.save_database("data/")
