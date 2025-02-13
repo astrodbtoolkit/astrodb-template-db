48485c23883474948281d21e320f6a04afe632b9
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2025-02-13 16:06:37 +0000

    updating ingest Gl229b script

--- scripts/ingest_gl229b.py
+++ scripts/ingest_gl229b.py
@@ -11,23 +11,46 @@ from astrodb_utils.publications import ingest_publication
 # Load the database
 DB_NAME = "tests/astrodb_template_tests.sqlite"
 SCHEMA_PATH = "schema/schema.yaml"
-db = load_astrodb(DB_NAME, recreatedb=True, felis_schema=SCHEMA_PATH)
+REFERENCE_TABLES = [
+    "Publications",
+    "Telescopes",
+    "Instruments",
+    "PhotometryFilters",
+    "Versions",
+    "Regimes",
+    "AssociationList",
+    "SourceTypeList"
+]
+db = load_astrodb(DB_NAME, recreatedb=True, felis_schema=SCHEMA_PATH, reference_tables=REFERENCE_TABLES)
 
-ingest_publication(db, doi="10.1038/378463a0")
+# ingest_publication(db, doi="10.1038/378463a0")
+# ingest_publication(db, doi="10.1086/498563")  # Burg06
 
-ingest_source(db, "Gl 229b", reference="Naka95")
+# ingest_source(db, "Gl 229b", reference="Naka95")
 
-companion_data = [
-    {
-        "source": "Gl 229b",
-        "companion": "Gl 229",
-        "relationship": "Child",
-        "reference": "Naka95",
-    },
+# companion_data = [
+#     {
+#         "source": "Gl 229b",
+#         "companion": "Gl 229",
+#         "relationship": "Child",
+#         "reference": "Naka95",
+#     },
+# ]
+
+# with db.engine.connect() as conn:
+#     conn.execute(db.CompanionRelationships.insert().values(companion_data))
+#     conn.commit()
+
+source_type_list_data = [
+    {"source_type": "T7 Dwarf"},
+]
+source_types_data = [
+    {"source": "Gl 229b", "source_type": "T7 Dwarf", "reference": "Burg06"},
 ]
 
 with db.engine.connect() as conn:
-    conn.execute(db.CompanionRelationships.insert().values(companion_data))
+    conn.execute(db.SourceTypeList.insert().values(source_type_list_data))
+    conn.execute(db.SourceTypes.insert().values(source_types_data))
     conn.commit()
-
+ 
 db.save_database("data/")
