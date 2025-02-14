80af12dbc638a4ef5656348a62959e84b84440af
kelle <kellecruz@gmail.com>
2025-02-13 21:50:06 -0500

    new scheduled tests

--- tests/scheduled_checks.py
+++ tests/scheduled_checks.py
@@ -12,21 +12,25 @@ from astroquery.simbad import Simbad
 
 from astrodbkit.utils import _name_formatter
 
+
 def test_SIMBAD_resolvable(db):
     # Verify that sources have SIMBAD-resolvable names```
-    results = db.query(db.Sources.c.source).all()
-    name_list = [s[0] for s in results]
+    all_sources = db.query(db.Sources.c.source).all()
+    name_list = [s[0] for s in all_sources]
+    # print(f"SIMBAD name list: {name_list}")
 
     # Include all SIMBAD Identifiers and our provided name to the SIMBAD query results table.
     Simbad.add_votable_fields("ids")
-    Simbad.add_votable_fields("typed_id")
 
     simbad_results = Simbad.query_objects(name_list)
     no_result_rows = []
     duplicate_rows = []
-    for row in simbad_results[["TYPED_ID", "IDS"]].iterrows():
+    no_db_matches = []
+    unique_matches = []
+    for row in simbad_results[["main_id", "ids"]].iterrows():
         try:
             name, ids = row[0].decode("utf-8"), row[1].decode("utf-8")
+            # print(f"SIMBAD name: {name}")
         except AttributeError:
             # Catch decoding error
             name, ids = row[0], row[1]
@@ -38,14 +42,26 @@ def test_SIMBAD_resolvable(db):
         ]
         if len(simbad_names) == 0:
             no_result_rows += [row]
-
+            print(f"No SIMBAD names for {name}")
 
         # Examine DB for each input, displaying results when more than one source matches
         t = db.search_object(
             simbad_names, output_table="Sources", fmt="astropy", fuzzy_search=False
         )
-        if len(t) != 1:
+        if len(t) > 1:
             duplicate_rows += [row]
+            print(f"Duplicate rows for {name}\n")
+            print(f"   Searched simbad names: {simbad_names}")
+            print(f"   Database matches: \n {t}")
+        elif len(t) == 0:
+            no_db_matches += [row]
+            print(f"No database matches for {name}")
+        else:
+            unique_matches += [row]
 
-    assert len(no_result_rows) == 0 and len(duplicate_rows) == 0, (f"No SIMBAD names for {no_result_rows},"
-                                                                   f" duplicate rows for {duplicate_rows}")
+    assert len(no_result_rows) == 0, f"No SIMBAD names for {no_result_rows}"
+    assert len(duplicate_rows) == 0, f" duplicate rows for {duplicate_rows}"
+    assert len(no_db_matches) == 0, f"No database matches for {no_db_matches}"
+    assert len(unique_matches) == len(
+        all_sources
+    ), f"Unique matches for {unique_matches}, expected {len(all_sources)}"
